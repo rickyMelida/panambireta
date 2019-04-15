6e7d242fe038a0cd96c82bd3478190a469471da2
Ricardo Mélida <ricardomelida92@gmail.com>
2019-04-15 16:51:36 -0400

    Se añade script d Jquey para efecto scroll, tambien se añade estilo a la mision, vision y valores

--- js/design.js
+++ js/design.js
@@ -1,8 +1,61 @@
 window.onload = function() {
-	ale
 	var valor_top = 100;
-	for (var i = 1; i <= 3; i++) {
+	for (var i = 1; i < 4; i++) {
 		$("#texto_"+i).css('top', valor_top + 'vh');
-		valor_top = valor_top + 100;	
+		valor_top = valor_top + 120;	
 	}
+
+
 }
+
+/*Cuando alcanza los valores relativos de las variables 
+estas van a desaparecer de la pantalla*/
+var sale_banner = 50;
+var sale_mision = 550;
+var sale_vision = 1050;
+var sale_valores = 1550;
+
+
+var banner = "#texto_0";
+var mision = "#texto_1";
+var vision = "#texto_2";
+var valores = "#texto_3";
+
+
+$(window).scroll(function(){
+	var valor_scroll = $(window).scrollTop();
+	console.log("el scroll vale " + valor_scroll);
+
+	//Si el valor del scroll es menor al valor donde tiene que desaparecer el banner
+	//Se mostrará el banner
+	if(valor_scroll < sale_banner) {
+		$(banner).fadeIn(1000, "linear");
+
+	}else {
+		$(banner).fadeOut(1000, "linear");
+
+	}
+
+	if(valor_scroll < sale_mision) {
+		$(mision).fadeIn(1000, "linear");
+
+	}else {
+		$(mision).fadeOut(1000, "linear");
+
+	}
+
+	if(valor_scroll < sale_vision) {
+		$(vision).fadeIn(1000, "linear");
+
+	}else {
+		$(vision).fadeOut(1000, "linear");
+		
+	}
+
+	if(valor_scroll < sale_valores) {
+		$(valores).fadeIn(1000, "linear");
+
+	}else {
+		$(valores).fadeOut(1000, "linear");
+	}
+});
