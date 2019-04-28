d2187bf09e2c95c115264c739a0f9cf66b474a09
Sub Estacion <subestacion@paseolagaleria.com>
2019-04-28 05:37:36 -0400

    Se automatiza el paso de las imagenes

--- js/design.js
+++ js/design.js
@@ -2,9 +2,8 @@ var siguiente = $("#btn-next");
 var anterior = $("#btn-prev");
 
 $("#slider").css("margin-left", "-" + 100 + "%");
-//$("#slider .portada:last").insertBefore("#slider .portada:first");
 
-
+/*Va a izquierda*/
 anterior.click(function() {
 	$("#slider").animate({marginLeft: "-" + 200 + "%"}, 700, function(){
 		$("#slider .portada:first").insertAfter("#slider .portada:last");
@@ -12,13 +11,21 @@ anterior.click(function() {
 	});
 });
 
+/*Va a derecha*/
 siguiente.click(function() {
-	$("#slider").animate({marginLeft: 0 + "%"}, 700, function(){
+	$("#slider").animate({marginLeft: 0 + "%"}, 2000, function(){
 		$("#slider .portada:last").insertBefore("#slider .portada:first");
 		$("#slider").css("margin-left", "-" + 100 + "%");
 	});
 });
 
+/*Pasa automatico las imagenes*/
+setInterval(function(){
+	$("#slider").animate({marginLeft: 0 + "%"}, 700, function(){
+		$("#slider .portada:last").insertBefore("#slider .portada:first");
+		$("#slider").css("margin-left", "-" + 100 + "%");
+	});
+}, 4000);
 
 function radio() {
 	window.open("http://listen.radionomy.com/panambireta", "Radio PanambiReta", "width=300, height=auto")
